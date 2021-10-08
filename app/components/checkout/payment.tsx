fb43eee46cf28bf84759441c5a4e2f4249152b5d
Ash Bhimasani <16abhimasani@gmail.com>
2021-10-08 02:32:13 -0400

    REFACTOR payment logic

---
+++ app/components/checkout/payment.tsx
@@ -0,0 +1,136 @@
+import React, { useState, useMemo, useCallback, FC } from 'react';
+import styled from 'styled-components';
+import { SlimSectionBody } from '.';
+
+export type ProductsType =
+  | 'PRINT_PAPER_HASH'
+  | 'PRINT_FRAME_HASH'
+  | 'PRINT_PAPER_LONDON'
+  | 'PRINT_FRAME_LONDON'
+  | 'FAILURE_TO_LAUNCH';
+
+export const PRICING = {
+  PRINT_PAPER_HASH: 0.08,
+  PRINT_FRAME_HASH: 0.25,
+  PRINT_PAPER_LONDON: 0.08,
+  PRINT_FRAME_LONDON: 0.25,
+  FAILURE_TO_LAUNCH: 111111,
+};
+
+const usePaymentFlow = (product: ProductsType) => {
+  const price = PRICING[product];
+  const slippage = 0.05;
+
+  const token = '$LONDON';
+  const rate = 0.0000123; // fake eth rate
+  const amountDue = (price / rate).toFixed(0);
+
+  const handlePay = useCallback(async () => {
+    console.log('calculate payment stub');
+    console.log('approve london tokens needed');
+    console.log('start contract interaction to accept payment');
+  }, []);
+
+  return { amountDue, price, rate, token, slippage, handlePay };
+};
+
+export const PaymentFlow: FC<{ product: ProductsType; disabled: boolean }> = ({
+  product,
+  disabled,
+}) => {
+  const [hoverPurchaseButton, setHoverPurchaseButton] = useState(false);
+  const { amountDue, handlePay, token, slippage } = usePaymentFlow(product);
+
+  const purchaseButton = useMemo(() => {
+    if (hoverPurchaseButton) {
+      if (disabled) {
+        return {
+          color: RED,
+          text: 'Not Ready',
+          underline: false,
+          disabled: true,
+        };
+      }
+    }
+    return {
+      color: GREEN,
+      text: 'Purchase Now',
+    };
+  }, [hoverPurchaseButton, disabled]);
+
+  const purchaseButtonOnClick = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    handlePay();
+  }, [disabled]);
+
+  return (
+    <>
+      <Price>
+        {amountDue} {token}
+        {/* <Slippage>Slip 5%</Slippage> */}
+      </Price>
+      <PurchaseButton
+        onClick={purchaseButtonOnClick}
+        onMouseEnter={() => setHoverPurchaseButton(true)}
+        onMouseLeave={() => setHoverPurchaseButton(false)}
+        style={{
+          background: purchaseButton.color,
+          textDecoration: purchaseButton.underline ? 'underline' : 'none',
+          cursor: purchaseButton.disabled ? 'not-allowed' : 'pointer',
+        }}
+      >
+        {purchaseButton.text}
+      </PurchaseButton>
+    </>
+  );
+};
+
+const GREEN = '#44db5e';
+const RED = '#FF6565';
+
+const PurchaseButton = styled(SlimSectionBody)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Helvetica;
+  font-style: normal;
+  font-weight: lighter;
+  font-size: 16px;
+  letter-spacing: 1px;
+  line-height: 18px;
+  text-align: center;
+  text-transform: uppercase;
+  color: #000000;
+  cursor: pointer;
+`;
+
+const Price = styled(SlimSectionBody)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Helvetica;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 18px;
+  text-align: center;
+  color: #000000;
+  position: relative;
+  span {
+    font-size: 12px;
+    opacity: 0.5;
+  }
+`;
+
+const Slippage = styled.div`
+  position: absolute;
+  bottom: 4px;
+  right: 6px;
+  font-size: 10px;
+  font-weight: normal;
+  color: black;
+  opacity: 0.4;
+  text-transform: uppercase;
+`;
--- app/components/checkout/print.tsx
+++ app/components/checkout/print.tsx
@@ -1,65 +1,19 @@
 import { useWeb3React } from '@web3-react/core';
-import React, { useState, useEffect, useMemo, useCallback, FC } from 'react';
-import styled from 'styled-components';
-import { RightSection, SectionBody, SlimSectionBody } from '.';
+import React, { useState, useMemo, FC } from 'react';
+import { RightSection, SectionBody } from '.';
 import { useAccountCollections } from '../../hooks/useCollection';
+import { PaymentFlow, ProductsType } from './payment';
 
 export const PrintCheckout = () => {
-  const [printOnly, setPrintOnly] = useState(true);
-  const [frameOption, setFrameOption] = useState<'framed' | 'paper'>('paper');
-  const handleFrameChange = (e: any) => setFrameOption(e.target.value);
-  useEffect(() => {
-    if (frameOption === 'paper') {
-      setPrintOnly(true);
-    }
-    if (frameOption === 'framed') {
-      setPrintOnly(false);
-    }
-  }, [frameOption]);
-  const price = printOnly ? 0.08 : 0.25;
+  const { account } = useWeb3React();
+  const collections = useAccountCollections(account);
 
   const [artwork, setArtworkOption] = useState<'london' | 'hash'>('london');
   const handleArtChange = (e: any) => setArtworkOption(e.target.value);
 
   const [artworkID, setArtworkID] = useState<string>('');
   const handleArtIDChange = (e: any) => setArtworkID(e.target.value);
-
-  const [email, setEmail] = useState<string>('');
-  const handleEmailChange = (e: any) => setEmail(e.target.value);
-
-  const [shipping, setShipping] = useState<string>('');
-  const handleShippingChange = (e: any) => setShipping(e.target.value);
-
-  const [hoverPurchaseButton, setHoverPurchaseButton] = useState(false);
-  const purchaseButton = useMemo(() => {
-    const green = '#44db5e';
-    const grey = '#FF6565';
-    if (hoverPurchaseButton) {
-      if (!email || !shipping || !artwork || !artworkID) {
-        return {
-          color: grey,
-          text: 'Not Ready',
-          underline: false,
-          disabled: true,
-        };
-      }
-    }
-    return {
-      color: green,
-      text: 'Purchase Print',
-    };
-  }, [hoverPurchaseButton, artwork, artworkID, email, shipping]);
-  const purchaseButtonOnClick = useCallback(() => {
-    if (!email || !shipping || !artwork || !artworkID) {
-      return;
-    }
-  }, [artwork, artworkID, email, shipping]);
-
-  const { account } = useWeb3React();
-  const collections = useAccountCollections(account);
-  console.log(collections);
-
-  const ArtworkSelectValues: FC = () => {
+  const ArtworkIDSelectValues: FC = () => {
     if (artwork === 'hash') {
       return (
         <>
@@ -80,26 +34,40 @@ export const PrintCheckout = () => {
     );
   };
 
+  const [frameOption, setFrameOption] = useState<'framed' | 'paper'>('paper');
+  const handleFrameChange = (e: any) => setFrameOption(e.target.value);
+
+  const [email, setEmail] = useState<string>('');
+  const handleEmailChange = (e: any) => setEmail(e.target.value);
+
+  const [shipping, setShipping] = useState<string>('');
+  const handleShippingChange = (e: any) => setShipping(e.target.value);
+
+  const reduceProduct = useMemo((): ProductsType => {
+    if (artwork === 'london') {
+      if (frameOption === 'framed') {
+        return 'PRINT_FRAME_LONDON';
+      }
+      return 'PRINT_PAPER_LONDON';
+    }
+
+    if (artwork === 'hash') {
+      if (frameOption === 'framed') {
+        return 'PRINT_FRAME_HASH';
+      }
+      return 'PRINT_PAPER_HASH';
+    }
+
+    return 'FAILURE_TO_LAUNCH';
+  }, [artwork, frameOption]);
+
+  const paymentDisabled =
+    !artwork || !artworkID || !frameOption || !email || !shipping;
+
   return (
     <>
       <RightSection>
-        <Price>
-          {/* {price} ETH  */}1 POSTER = {(price / 0.0000123).toFixed(0)}{' '}
-          $LONDON
-          <Slippage>Slip 5%</Slippage>
-        </Price>
-        <PurchaseButton
-          onClick={purchaseButtonOnClick}
-          onMouseEnter={() => setHoverPurchaseButton(true)}
-          onMouseLeave={() => setHoverPurchaseButton(false)}
-          style={{
-            background: purchaseButton.color,
-            textDecoration: purchaseButton.underline ? 'underline' : 'none',
-            cursor: purchaseButton.disabled ? 'not-allowed' : 'pointer',
-          }}
-        >
-          {purchaseButton.text}
-        </PurchaseButton>
+        <PaymentFlow product={reduceProduct} disabled={paymentDisabled} />
       </RightSection>
 
       <RightSection>
@@ -115,7 +83,7 @@ export const PrintCheckout = () => {
           <h4>Select Artwork</h4>
           <br />
           <select onChange={(e) => handleArtIDChange(e)}>
-            <ArtworkSelectValues />
+            <ArtworkIDSelectValues />
           </select>
         </SectionBody>
       </RightSection>
@@ -124,10 +92,7 @@ export const PrintCheckout = () => {
         <SectionBody>
           <h4>Choose Package</h4>
           <br />
-          <select
-            // style={{ width: '50%' }}
-            onChange={(e) => handleFrameChange(e)}
-          >
+          <select onChange={(e) => handleFrameChange(e)}>
             <option value="paper">
               No Frame / Print Only - Ships in 2 Weeks
             </option>
@@ -166,47 +131,3 @@ export const PrintCheckout = () => {
     </>
   );
 };
-
-const Price = styled(SlimSectionBody)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-family: Helvetica;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 18px;
-  text-align: center;
-  color: #000000;
-  position: relative;
-  span {
-    font-size: 12px;
-    opacity: 0.5;
-  }
-`;
-const Slippage = styled.div`
-  position: absolute;
-  bottom: 4px;
-  right: 6px;
-  font-size: 10px;
-  font-weight: normal;
-  color: black;
-  opacity: 0.4;
-  text-transform: uppercase;
-`;
-
-const PurchaseButton = styled(SlimSectionBody)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-family: Helvetica;
-  font-style: normal;
-  font-weight: lighter;
-  font-size: 16px;
-  letter-spacing: 1px;
-  line-height: 18px;
-  text-align: center;
-  text-transform: uppercase;
-  color: #000000;
-  cursor: pointer;
-`;
