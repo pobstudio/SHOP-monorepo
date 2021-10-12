d874759e505bc35d17703c89c0ed522668792f98
Ash Bhimasani <16abhimasani@gmail.com>
2021-10-12 09:26:31 -0400

    REMOVE Open Piano protocol code

--- protocol/typechain/index.ts
+++ protocol/typechain/index.ts
@@ -7,7 +7,6 @@ export type { IERC165 } from './IERC165';
 export type { IERC721 } from './IERC721';
 export type { IERC721Metadata } from './IERC721Metadata';
 export type { IERC721Receiver } from './IERC721Receiver';
-export type { OpenPiano } from './OpenPiano';
 
 export { ERC165__factory } from './factories/ERC165__factory';
 export { ERC721__factory } from './factories/ERC721__factory';
@@ -15,4 +14,3 @@ export { IERC165__factory } from './factories/IERC165__factory';
 export { IERC721__factory } from './factories/IERC721__factory';
 export { IERC721Metadata__factory } from './factories/IERC721Metadata__factory';
 export { IERC721Receiver__factory } from './factories/IERC721Receiver__factory';
-export { OpenPiano__factory } from './factories/OpenPiano__factory';
