6620a55ef722bc120b800673363a4a828d779756
Ash Bhimasani <16abhimasani@gmail.com>
2021-11-10 21:05:22 -0500

    UPDATE faq

--- app/pages/faq.tsx
+++ app/pages/faq.tsx
@@ -25,80 +25,19 @@ const FaqPage: NextPage = () => {
           <PageTitle>FAQ</PageTitle>
           <PageBody>
             <PageBodySection>
-              <PageSubheader>Order Issues</PageSubheader>
+              <PageSubheader>Can I print other art?</PageSubheader>
               <PageText>
-                1. Join the official POB Discord:{' '}
-                <a
-                  href={`https://discord.gg/pob`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  discord.gg/pob
-                </a>
-                <br />
-                2. Follow the necessary steps to view all channels in the
-                General section.
-                <br />
-                3. In the #support channel, please use the
-                <a
-                  href="https://discord.com/channels/797978413691305995/798782663313195020/874358061840601098"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {' '}
-                  Support bot
-                </a>{' '}
-                and file a ticket.
-                <br />
-                4. Include as much information as possible like your payment's
-                transaction id, the info you used during checkout, the link to
-                your artwork, your Twitter or Discord handle etc.
+                Not just yet. We will work on expanding to other artists
+                eventually. Tweet at us and your favorite collection to gauge
+                interest. We would love to grow the Print Service.
               </PageText>
             </PageBodySection>
 
             <PageBodySection>
-              <PageSubheader>Shipping Information</PageSubheader>
+              <PageSubheader>Can I print in other sizes?</PageSubheader>
               <PageText>
-                All orders include free international delivery. Please use an
-                address that you are confident will register in{' '}
-                <a
-                  href="https://www.fedex.com/en-us/online/rating.html#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  FedEx
-                </a>
-                . Shipping a framed piece requires meticulously wrapping the
-                frame and packing everything graciously for transport; this is
-                very expensive. Paper only orders are shipped in hard, plastic
-                canisters and are much easier overall to process.
-              </PageText>
-            </PageBodySection>
-
-            <PageBodySection>
-              <PageSubheader>Contact Us</PageSubheader>
-              <PageText>
-                <a
-                  href={`mailto:team@pob.studio`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Email
-                </a>{' '}
-                <a
-                  href={`https://twitter.com/prrfbeauty`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Twitter
-                </a>{' '}
-                <a
-                  href={`https://discord.gg/pob`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Discord
-                </a>{' '}
+                To standardize how our art is displayed, we only provide 1 size
+                for HASH and LONDON GIFT respectively.
               </PageText>
             </PageBodySection>
           </PageBody>
